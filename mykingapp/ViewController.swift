9fe6ba4e4ab28ba5edd8a144f6cb584cf73503c6
Spencer <Spencer.creveling@gmail.com>
2019-01-31 14:44:42 -0500

    I tested the commit function

--- mykingapp/ViewController.swift
+++ mykingapp/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello!")
+        print ("spencer was here")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
