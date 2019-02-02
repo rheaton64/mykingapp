8ca9719f9365d59f3091042ba8e020cac09b4202
Spencer <Spencer.creveling@gmail.com>
2019-02-01 22:26:55 -0500

    I created the Class Tab UI along with sample data. I also created the more info view and added basic passing of data that can be built upon once the have the Json file

---
+++ mykingapp/Classes/MoreClassInfo.swift
@@ -0,0 +1,38 @@
+//
+//  MoreClassInfo.swift
+//  mykingapp
+//
+//  Created by Spencer Creveling on 2/1/19.
+//  Copyright © 2019 Ryan Heaton. All rights reserved.
+//
+
+import UIKit
+
+class MoreClassInfo: UIViewController {
+    //reciving data
+    var passedName = " "
+    var passedColor = UIColor.black
+    
+    
+    @IBOutlet weak var selectedClassName: UILabel!
+    
+    override func viewDidLoad() {
+        selectedClassName.text = passedName
+        selectedClassName.backgroundColor = passedColor
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
