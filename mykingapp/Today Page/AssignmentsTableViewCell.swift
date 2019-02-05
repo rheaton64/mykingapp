f451c10ec477a4edb2530fb84f55a98f3cc0ddfb
Luke Nascimento <lnascimento@kingschoolct.org>
2019-02-05 12:40:02 -0500

    This is the right one

--- mykingapp/Today Page/AssignmentsTableViewCell.swift
+++ mykingapp/Today Page/AssignmentsTableViewCell.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 
-class AssignmentsTableViewCell: UITableViewCell, UITableViewDelegate, UITableViewDataSource {
+class AssignmentsTableViewCell: UITableViewCell /* , UITableViewDelegate, UITableViewDataSource */ {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         <#code#>
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         <#code#>
-    }
+    }*/
     
 
     override func awakeFromNib() {
