4fa017ea8e15970cd1920dd448f38cb2f0caff59
Natasha Aysseh <naysseh@aol.com>
2019-03-08 12:48:57 -0500

    made new class for structs

---
+++ mykingapp/studentDataStructs.swift
@@ -0,0 +1,23 @@
+//
+//  studentDataStructs.swift
+//  mykingapp
+//
+//  Created by Natasha  on 3/8/19.
+//  Copyright © 2019 Ryan Heaton. All rights reserved.
+//
+
+import Foundation
+
+struct studentData {
+    var student_name: String
+    
+}
+
+struct assignmentData {
+    var className: String
+    var assignmentTitle: String
+    var assignDescript: String
+    var assignDate: String
+    
+}
+
