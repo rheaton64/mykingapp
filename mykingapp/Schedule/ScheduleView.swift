145d2ca2b7128c57912dfed745666a4b8477b618
Spencer <Spencer.creveling@gmail.com>
2019-03-28 12:27:05 -0400

    decoding Json

---
+++ mykingapp/Schedule/ScheduleView.swift
@@ -0,0 +1,35 @@
+//
+//  ScheduleView.swift
+//  mykingapp
+//
+//  Created by Spencer Creveling on 3/28/19.
+//  Copyright © 2019 Ryan Heaton. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleView: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+   
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      
+        return 7
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+}
