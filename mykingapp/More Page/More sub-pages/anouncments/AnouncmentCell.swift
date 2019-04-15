5b0547fde01b38a3faaf378ce476934cda1f879e
Spencer <Spencer.creveling@gmail.com>
2019-04-15 12:01:48 -0400

    made part of the cell

---
+++ mykingapp/More Page/More sub-pages/anouncments/AnouncmentCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnouncmentCell.swift
+//  mykingapp
+//
+//  Created by Spencer Creveling on 4/12/19.
+//  Copyright © 2019 Ryan Heaton. All rights reserved.
+//
+
+import UIKit
+
+class AnouncmentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
